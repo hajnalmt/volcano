3bb1fa300c9bd30cf3b3e3a82ba1592b899fb95d
Hajnal Máté <hajnalmt@gmail.com>
2026-01-05 15:17:01 +0100

    fix: victim taskorder fn
    Signed-off-by: Hajnal Máté <hajnalmt@gmail.com>

--- pkg/scheduler/api/types.go
+++ pkg/scheduler/api/types.go
@@ -127,6 +127,9 @@ type CompareFn func(interface{}, interface{}) int
 // VictimCompareFn is the func declaration used by sort or priority victims.
 type VictimCompareFn func(interface{}, interface{}, interface{}) int
 
+// VictimCompareFn is the func declaration used by sort or priority victims.
+type VictimTaskCompareFn func(interface{}, interface{}, interface{}) int
+
 // ValidateFn is the func declaration used to check object's status.
 type ValidateFn func(interface{}) bool
 
